8658391f7d351a686ea413fe1f325ebf2bcedc87
sixertoy <sixertoy.github@gmail.com>
2015-08-04 11:58:15 +0200

    add modal styles + rename in import project

--- src/html/js/app/sidebar/controllers/modal-controller.js
+++ src/html/js/app/sidebar/controllers/modal-controller.js
@@ -8,29 +8,13 @@
         .controller('NewProjectModalController', ['$scope', '$modalInstance', function ($scope, $modalInstance) {
 
             $scope.data = {
-                project_uri: ''
+                project_uri: '',
+                project_name: ''
             };
 
             $scope.onModalSubmit = function () {
                 if ($scope.data.project_uri !== '') {
-                    $modalInstance.close($scope.data.project_uri);
-                }
-            };
-
-            $scope.onModalCancel = function () {
-                $modalInstance.dismiss('cancel');
-            };
-
-        }])
-        .controller('RenameProjectModalController', ['$scope', '$modalInstance', 'project_name', function ($scope, $modalInstance, project_name) {
-
-            $scope.data = {
-                project_name: project_name
-            };
-
-            $scope.onModalSubmit = function () {
-                if ($scope.data.project_name !== '') {
-                    $modalInstance.close($scope.data.project_name);
+                    $modalInstance.close($scope.data.project_uri, $scope.data.project_name);
                 }
             };
 
