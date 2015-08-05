670be871a4e45f939326d6d0c8ae06d23cd844cc
sixertoy <sixertoy.github@gmail.com>
2015-08-05 19:23:57 +0200

    update viewer editor

--- src/html/js/app/viewer/viewer-editor-controller.js
+++ src/html/js/app/viewer/viewer-editor-controller.js
@@ -5,12 +5,24 @@
     'use strict';
 
     angular.module('readmepadAppViewer')
-        .controller('EditorController', ['$scope', 'document', function ($scope, document) {
+        .controller('EditorController', ['$scope', '$http', 'document', function ($scope, $http, document) {
 
+            console.log($http);
 
-            $scope.document = document;
-            console.log($scope.document);
-
+        }])
+        .config(['$stateProvider', '$urlRouterProvider', '$base64', function ($stateProvider, $urlRouterProvider, $base64) {
+            $stateProvider
+                .state('document', {
+                    url: '/document/:id',
+                    controller: 'EditorController',
+                    templateUrl: 'viewer/editor.html',
+                    resolve: {
+                        document: function ($stateParams) {
+                            return $base64.decode(decodeURIComponent($stateParams.id));
+                        }
+                    }
+                });
+            $urlRouterProvider.otherwise('/');
         }]);
 
 }());
